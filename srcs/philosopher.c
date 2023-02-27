9208173264ee1b4dd442d0fac85e114c88ed4514
aurelien <aurelien.calandrino@outlook.fr>
2023-02-27 15:27:24 +0100

    Lazy-commit

--- srcs/philosopher.c
+++ srcs/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: aucaland <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 10:31:11 by aurel             #+#    #+#             */
-/*   Updated: 2023/02/27 14:13:43 by aucaland         ###   ########.fr       */
+/*   Updated: 2023/02/27 14:21:24 by aucaland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ t_error	philo_spawn(t_parent *parent, int nbr_of_philo)
 int main(int argc, char **argv)
 {
 	t_parent	parent;
+	pthread_mutex_t
 
-	if (argc < 5 && argc > 6)
+	if (argc < 5 && argc > 6) //TODO: create a check function for each arg
 		return (printf("%s", INV_ARGUMENTS));
-
 	ft_bzero(&parent, sizeof(t_parent));
+	parent.number_of_philo = ft_atoi(argv[1]);
 	if (philo_spawn(&parent) == MALLOC_ERROR)
 		exit_malloc(&parent);
 
